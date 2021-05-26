b0edc30f068752627f49e8a081e17cf395228289
daehwan <daewhan5024@gmail.com>
2021-05-26 15:38:07 +0900

    Done

--- main.py
+++ main.py
@@ -1,4 +1,44 @@
-import numpy
 import cv2
-import cv2.aruco as acruco
+import cv2.aruco as aruco
 from djitellopy import tello
+from time import sleep
+import numpy as np
+
+goal_x = 640
+goal_y = 360
+
+def findaruco(img, markerSize = 6, totalMarkers=50, draw=True):
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    key = getattr(aruco, f'DICT_{markerSize}X{markerSize}_{totalMarkers}')
+    arucoDict = aruco.Dictionary_get(aruco.DICT_4X4_50)
+    arucoParam = aruco.DetectorParameters_create()
+    bboxs, ids, rejected = aruco.detectMarkers(imgGray, arucoDict, parameters=arucoParam)
+    return ids, bboxs
+
+def getWhere(bboxs):
+
+
+def main():
+    cap = cv2.VideoCapture(0)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+    cap.set(cv2.CAP_PROP_XI_FRAMERATE, 60.0)
+    while(True):
+        ret, img = cap.read()
+        cv2.imshow('test', img)
+        ids, bboxs = findaruco(img)
+
+        if(type(ids)!='NoneType' and type(bboxs)!='NoneType'):
+            print(bboxs[0][0][0][0]+bboxs[0][0][0][1])
+            print(bboxs[0][0][1][0]+bboxs[0][0][1][1])
+            print(bboxs[0][0][1][0]+bboxs[0][0][1][1])
+            print(bboxs[0][0][1][0]+bboxs[0][0][1][1])
+
+
+        if cv2.waitKey(1) & 0xff == ord('q'):
+            break
+
+
+
+if __name__ == "__main__":
+    main()
