594ae51ba499d4038dcd684a3571f2f947d73956
daehwan <daewhan5024@gmail.com>
2021-06-16 16:36:07 +0900

    201-06-16_Remark

--- PID_Controll.py
+++ PID_Controll.py
@@ -2,17 +2,18 @@ import numpy as np
 
 class controll:
     def __init__(self, goal, K_PID, dt):
-        self.goal = goal
-        self.K = K_PID
-        self.dt = dt
-        self.previous = np.array([0,0])
-        self.Integral = np.array([0,0])
+        self.goal = goal #목표 위치
+        self.K = K_PID #PID제어를 위한 비례상수 1*3크기이며 순서대로 K_p, K_i, K_d이다
+        self.dt = dt #시간 간격으로 1/카메라의 프레임수 이다.
+        self.previous = np.array([0,0]) #바로 전의 값을 저장한다.
+        self.Integral = np.array([0,0]) #현재 까지의 적분값을 저장한다.
         print(self.dt)
 
     def get_speed(self, location):
-        self.factor = (self.goal - location)
-        self.Integral = self.Integral+self.factor*self.dt
-        self.velocity = self.factor*self.K[0] + self.Integral*self.K[1] + ((self.factor - self.previous)/self.dt)*self.K[2]
-        self.previous = self.factor
+        self.factor = (self.goal - location) #오차 계산
+        self.velocity = self.factor*self.K[0] + self.Integral*self.K[1] +\
+                        ((self.factor - self.previous)/self.dt)*self.K[2] #PID제어를 위한 값 계산
+        self.Integral = self.Integral+self.factor*self.dt #적분 값 계산
+        self.previous = self.factor #바로 전의 값을 현재 값으로 업데이트
 
-        return self.velocity
+        return self.velocity #계산한 값 리턴
--- main.py
+++ main.py
@@ -4,27 +4,30 @@ from djitellopy import tello
 import numpy as np
 from PID_Controll import controll
 import time
-'''
+
 drone = tello.Tello()
+drone.connect()
 print(drone.get_battery())
 drone.send_rc_control(0,0,0,0)
-'''
+
 goal = np.array([640, 360])
 
 
 def findaruco(img, markerSize = 4, totalMarkers=50):
-    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    key = getattr(aruco, f'DICT_{markerSize}X{markerSize}_{totalMarkers}')
-    arucoDict = aruco.Dictionary_get(key)
-    arucoParam = aruco.DetectorParameters_create()
-    bboxs, ids, rejected = aruco.detectMarkers(imgGray, arucoDict, parameters=arucoParam)
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #회색 이미지로 변환
+    key = getattr(aruco,
+                  f'DICT_{markerSize}X{markerSize}_{totalMarkers}') #필요한 ArUco 마커 Dictionary이름 생성
+    arucoDict = aruco.Dictionary_get(key) #ArUco Dictionary 생성
+    arucoParam = aruco.DetectorParameters_create() #오차 보정을 위한 파라미터 생성
+    bboxs, ids, rejected = aruco.detectMarkers(imgGray, arucoDict,
+                                               parameters=arucoParam) #imgGray에서 ArUco 마커 탐색
     return ids, bboxs
 
 def getWhere(bbox):
     print(bbox)
     if(len(bbox)==0):
         return -1, -1
-    a = np.reshape(bbox, (2,4))
+    a = np.reshape(bbox, (2, 4))
     return np.sum(a, axis=1)/4
 
 
@@ -35,11 +38,11 @@ cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 cap.set(cv2.CAP_PROP_FPS, 30.0)
 
-controller = controll(goal, np.array([1,0.3,0.2]), 1/cap.get(cv2.CAP_PROP_FPS))
+controller = controll(goal, np.array([1, 0.3, 0.2]), 1/cap.get(cv2.CAP_PROP_FPS))
 
 frames = 0
 start = time.time()
-while(True):
+while True:
     ret, img = cap.read()
     frames = frames+1
     if not ret:
